5aebe6d6d3112540553cb5216b273fba37e074da
romulofull <romulo.castro@gmail.com>
2024-07-28 12:56:08 -0500

    Prompt y Switch

---
+++ condicionales.js
@@ -0,0 +1,24 @@
+var aprobe = true;
+if (aprobe == true) {
+    console.log("Puedo Salir!!!!");
+}
+else {
+    console.log("Tengo que estudiar");
+}
+
+
+var color = prompt ("Ingrese un color : ");
+switch(color) {
+case "rojo":
+console.log("Es Rojo");
+break;
+case "azul":
+console.log("Es Azul");
+break;
+case "amarillo":
+console.log("Es Amarillo");
+break;
+default:
+console.log("El color ingresado no es válido");
+break
+}
